60f542149a86a08cb2bb8be36f3a41eccba731bb
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2011-10-15 21:28:54 +0200

    Optional time parameters in CFlow (date, from_time, to_time, to_date) [fix]

--- webhomer/cflow.php
+++ webhomer/cflow.php
@@ -98,7 +98,7 @@ $cid2 = getVar('cid2', NULL, 'get', 'string');
 
 //Make image
 $where = "( callid = '".$cid."'";
-if(isset($cid2)) { $where .= " OR callid='".$cid2."')"; } else {  $where .= ") ";} ;
+if(isset($cid2)) { $where .= " OR callid='".$cid2."')"; } else {  $where .= ") ";} 
 
 //Crop Search Parameters, if any
         $flow_date = getVar('date', NULL, 'get', 'string');
@@ -109,7 +109,7 @@ if(isset($cid2)) { $where .= " OR callid='".$cid2."')"; } else {  $where .= ") "
         if (isset($flow_date, $flow_from_time, $flow_to_time))
         {
         	$ft = date("Y-m-d H:i:s", strtotime($flow_date." ".$flow_from_time));
-		if (isset($flow_to_date) { $flow_date = $flow_to_date; } 
+		if (isset($flow_to_date)) { $flow_date = $flow_to_date; } 
         	$tt = date("Y-m-d H:i:s", strtotime($flow_date." ".$flow_to_time));
         	$timewhere = "(`date` >= '$ft' AND `date` <= '$tt' )";
 	   // Hook to query
