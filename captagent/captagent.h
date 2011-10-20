76b154d6750ea623c9ec866c77a461456e0fb5bd
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2011-10-20 14:54:32 +0200

    captagent: add the new version of captagent: 0.7

---
+++ captagent/captagent.h
@@ -0,0 +1,33 @@
+
+#define VERSION "0.6"
+#define DEFAULT_CONFIG "/usr/local/etc/captagent/captagent.conf"
+#define DEFAULT_PIDFILE  "/var/run/captagent.pid"
+#define DEFAULT_PORT "5060"
+
+/* filter to extract SIP packets */
+char filter_expr[255];
+
+/* Ethernet / IP / UDP header IPv4 */
+const int udp_payload_offset = 14+20+8;
+
+
+struct hep_hdr{
+    u_int8_t hp_v;            /* version */
+    u_int8_t hp_l;            /* length */
+    u_int8_t hp_f;            /* family */
+    u_int8_t hp_p;            /* protocol */
+    u_int16_t hp_sport;       /* source port */
+    u_int16_t hp_dport;       /* destination port */
+};
+
+struct hep_iphdr{
+        struct in_addr hp_src;
+        struct in_addr hp_dst;      /* source and dest address */
+};
+
+#ifdef USE_IPV6
+struct hep_ip6hdr {
+        struct in6_addr hp6_src;        /* source address */
+        struct in6_addr hp6_dst;        /* destination address */
+};
+#endif
