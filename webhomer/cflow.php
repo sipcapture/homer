dafb06d6692051f77004c30c573ba7b6cbd81868
Alexandr.Dubovikov <Alexandr.Dubovikov@gmail.com>
2011-10-24 10:48:49 +0000

    we don't need CSS here. It has loaded in homer.html.php already.

--- webhomer/cflow.php
+++ webhomer/cflow.php
@@ -432,10 +432,10 @@ imagedestroy($im);
 ?>
 <html>
 <head>
+<!--
 <link href="styles/core_styles.css" rel="stylesheet" type="text/css" />
 <link href="styles/form.css" rel="stylesheet" type="text/css" />
 <link type="text/css" href="styles/jquery-ui-1.8.4.custom.css" rel="stylesheet" />
-<!--
 <script src="js/jquery-1.6.4.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
 -->
