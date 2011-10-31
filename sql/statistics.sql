f0051f9820988ce43a00ab28d24531c1ee7de9ae
Alexandr.Dubovikov <Alexandr.Dubovikov@gmail.com>
2011-10-31 15:21:54 +0000

    updated SQL for statistics

--- sql/statistics.sql
+++ sql/statistics.sql
@@ -1,36 +1,39 @@
 DROP TABLE stats_method;
 CREATE TABLE IF NOT EXISTS `stats_method` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `from_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `to_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `method` varchar(50) NOT NULL DEFAULT '',
-  `total` int(10) NOT NULL DEFAULT 0,
-  `auth` int(10) NOT NULL DEFAULT 0,
-  `completed` int(10) NOT NULL DEFAULT 0,
-  `uncompleted` int(10) NOT NULL DEFAULT 0,
-  `rejected` int(10) NOT NULL DEFAULT 0,
-  `asr` int(10) NOT NULL DEFAULT 0,
-  `ner` int(10) NOT NULL DEFAULT 0,
+  `total` int(10) NOT NULL DEFAULT '0',
+  `auth` int(10) NOT NULL DEFAULT '0',
+  `completed` int(10) NOT NULL DEFAULT '0',
+  `uncompleted` int(10) NOT NULL DEFAULT '0',
+  `rejected` int(10) NOT NULL DEFAULT '0',
+  `asr` int(10) NOT NULL DEFAULT '0',
+  `ner` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `date` (`date`),
+  UNIQUE KEY `datemethod` (`to_date`,`method`),
+  KEY `from_date` (`from_date`),
+  KEY `to_date` (`to_date`),
   KEY `method` (`method`),
   KEY `total` (`total`),
   KEY `completed` (`completed`),
-  KEY `uncompleted` (`uncompleted`),
-  UNIQUE KEY `datemethod` (`date`,`method`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
+  KEY `uncompleted` (`uncompleted`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
 DROP TABLE stats_useragent;
-CREATE TABLE IF NOT EXISTS `stats_useragent` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+CREATE TABLE IF NOT EXISTS `stats_useragent` {
+`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `from_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `to_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `useragent` varchar(100) NOT NULL DEFAULT '',
   `method` varchar(50) NOT NULL DEFAULT '',
-  `total` int(10) NOT NULL DEFAULT 0,
+  `total` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `date` (`date`),
+  UNIQUE KEY `datemethodua` (`to_date`,`method`,`useragent`),
+  KEY `from_date` (`from_date`),
+  KEY `to_date` (`to_date`),
   KEY `useragent` (`useragent`),
   KEY `method` (`method`),
-  KEY `total` (`total`),
-  UNIQUE KEY `datemethodua` (`date`,`method`,`useragent`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `total` (`total`)
+) ENGINE=MyISAM AUTO_INCREMENT=2771 DEFAULT CHARSET=latin1;
