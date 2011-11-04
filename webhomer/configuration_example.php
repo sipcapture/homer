61421d03261c28300053be29ddbe867b626253c0
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2011-11-04 18:33:53 +0100

    Charts alternative initial (work in progress)

--- webhomer/configuration_example.php
+++ webhomer/configuration_example.php
@@ -25,6 +25,7 @@ define(WEBPCAPLOC,"/webhomer/tmp/");
 define(APILOC,"/webhomer/api/");
 define(COLORTAG, 0); /* color based on callid, fromtag */
 define(DAYNIGHT, 1); /* day/night based theme, 0=day, 1=rotate, 3=night */
+define(CHARTER, 1); /* 1: Highcharts, 2: */
 define(AJAXTYPE,"POST"); /* AJAX request type can be POST or GET */
 
 
