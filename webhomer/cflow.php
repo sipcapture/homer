799c5bf66f589c047acc88f13c4cad1a895d082e
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2012-01-31 16:23:24 +0100

    Minor fix to Cflow search extension link (...)

--- webhomer/cflow.php
+++ webhomer/cflow.php
@@ -560,7 +560,7 @@ $(document).ready(function(){
     <input id="s2" type="button" value="PCAP" onclick="window.open('pcap.php?<?php echo $pcapurl; ?>');" style="background: transparent;"/>
 <?php  if (isset($flow_from_date)) { ?>
     <input type="button" value="Duration: <?php echo $totdur ?>" style="opacity: 1; background: transparent; background-color: <?php echo $statuscolor; ?>" disabled />
-    <input type="button" value="..." style="opacity: 1; background: transparent;" onclick="$(this).parent().parent().load('cflow.php?cid=<?php echo $cid ?>&b2b=<?php echo $b2b ?>');"/>
+    <input type="button" value="..." style="opacity: 1; background: transparent;" onclick="$(this).parent().parent().load('cflow.php?cid=<?php echo $cid ?>&b2b=<?php echo $b2b ?><?php foreach($location as $loc) { echo "&location[]=".$loc; } ?>&unique=<?php echo $unique ?>');"/>
 <?php } else {  ?>
     <input type="button" value="Duration: <?php echo $totdur ?>" style="opacity: 1; background: transparent; background-color: <?php echo $statuscolor; ?>" disabled />
 <?php   }  ?>
