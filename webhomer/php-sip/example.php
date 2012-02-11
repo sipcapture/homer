cb9b4fc21e229680207d0f4b7f37916ffbbec362
lorenzo.mangani <lorenzo.mangani@gmail.com>
2012-02-11 13:27:02 +0000

    Cleaning up this example

--- webhomer/php-sip/example.php
+++ webhomer/php-sip/example.php
@@ -11,11 +11,11 @@ $item = $_GET['from'];
 try
 {
   $api = new PhpSIP();
-  $api->setProxy('sipx.qxip.net:5060'); 
-  $api->addHeader('Event: homer');
+  $api->setProxy('sip.host.ext:5060'); 
+  $api->addHeader('Event: Homer');
   $api->setMethod('OPTIONS');
-  $api->setFrom('sip:4504@qxip.net');
-  $api->setUri('sip:4504@qxip.net');
+  $api->setFrom('sip:1234@host.ext');
+  $api->setUri('sip:1234@host.ext');
   $api->setUserAgent('HOMER SIPCAPTURE');
   $res = $api->send();
 
