b758e9a149bc050c2f61e5b2cf24d340eb9f108f
Alexandr.Dubovikov <Alexandr.Dubovikov@gmail.com>
2012-03-23 14:47:20 +0000

    changed version

--- captagent/captagent.h
+++ captagent/captagent.h
@@ -1,4 +1,4 @@
-#define VERSION "0.8.4"
+#define VERSION "0.8.5"
 #define DEFAULT_CONFIG "/usr/local/etc/captagent/captagent.ini"
 #define DEFAULT_PIDFILE  "/var/run/captagent.pid"
 #define DEFAULT_PORT "5060"
