1db52fa6f20dc434eee3370f0914629e540f7e10
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2012-05-03 18:56:54 +0200

    Adding tickbox in PCAP import UI to use HEPv2 and preserve timestamps

--- webhomer/utils.php
+++ webhomer/utils.php
@@ -255,8 +255,9 @@ function LoadPcap() {
 			?>
 			<div id="pcapout">
 			<form id="pcapup"  target="FileUpload" action="utils.php?task=pcapin" method="post" enctype="multipart/form-data">
-			<input type="file" name="file" id="file" /> 
-			<br />
+			<input type="file" name="file" id="file" /> <br>
+			<input type="checkbox" name="HEP2" id="HEP2" value="1"> PCAP Timestamps
+			<br /><br />
 			<input type="submit" name="submit" value="Start Upload" onclick="$('#FileUpload').show();" /> <img src="images/pcap.png" align="middle" style="margin: -6 2 0 0;"> 
 			</form>
 			</div>
@@ -269,15 +270,20 @@ function LoadPcap() {
 		 		echo "Error: " . $_FILES["file"]["error"] . "<br />"; 
 			} else {
 			        $pcapin = PCAPDIR . $_FILES["file"]["name"];
+				if(isset($_POST['HEP2']) &&
+   					$_POST['HEP2'] == '1')
+					{ $hepv = " -H 2 -i 101"; } else { $hepv = ""; }
 				$fext = substr($pcapin, strripos($pcapin, '.'));
 				if ($fext != '.pcap') {echo $fext." != .PCAP"; exit;}
 				move_uploaded_file($_FILES["file"]["tmp_name"], $pcapin );
 				if (!file_exists($pcapin)) { echo "File Horror!"; } else {
-					exec(PCAP_AGENT.' -P /tmp/captagent_in.pid -s '.PCAP_HEP_IP.' -p '.PCAP_HEP_PORT.' -D '.$pcapin, $result, $status);
+					exec(PCAP_AGENT.' -P /tmp/captagent_in.pid -s '.PCAP_HEP_IP.' -p '.PCAP_HEP_PORT.' -D '.$pcapin.' '.$hepv, $result, $status);
 		                        if ($status != 0) { echo "Agent Not Available. Install captagent";
 		                        } else { 
 						if ($result[0]!='') { echo $result[0]; } else {
-						echo "PCAP streamed to ".PCAP_HEP_IP.":".PCAP_HEP_PORT; }
+						echo "PCAP streamed to ".PCAP_HEP_IP.":".PCAP_HEP_PORT; 
+						if ($hepv != "") { echo "<br>PCAP Time Preserved"; }
+						}
 					}
 				}
 			} 
