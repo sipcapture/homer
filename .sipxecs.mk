e50046f945012c3552b7493e802aa664598094fb
Douglas Hubler <dhubler@ezuce.com>
2012-06-21 21:27:54 -0400

    hook build file for sipxecs

---
+++ .sipxecs.mk
@@ -0,0 +1,5 @@
+homer_VER = 3.0.0
+homer_REV = 1
+homer_SRPM = homer-$(homer_VER)-$(homer_REV)$(RPM_DIST).src.rpm
+homer_SPEC = $(SRC)/$(PROJ)/homer.spec
+homer_SOURCES = $(BUILDDIR)/$(PROJ)/homer-$(homer_VER).tar.gz
