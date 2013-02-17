ed8f19ffb209f52a6940d2dff7ddfd1c0d20294d
lorenzo.mangani <lorenzo.mangani@gmail.com>
2013-02-17 23:03:40 +0000

    Fixing the script to notice actual license

--- scripts/extra/homer_installer.sh
+++ scripts/extra/homer_installer.sh
@@ -17,19 +17,17 @@
 #
 #  This script is part of the HOMER project (http://sipcapture.org)
 #  The HOMER project is free software; you can redistribute it and/or 
-#  modify it under the terms of the GNU General Public License as 
-#  published by the Free Software Foundation; either version 2 of 
+#  modify it under the terms of the GNU Affero General Public License as 
+#  published by the Free Software Foundation; either version 3 of 
 #  the License, or (at your option) any later version.
 #
-#  The GNU General Public License can be found at
-#  http://www.gnu.org/copyleft/gpl.html. A copy is found in the 
-#  textfile GPL.txt and important notices to the license from the 
-#  author is found in LICENSE.txt distributed with these scripts.
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 #  This script is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#  GNU Affero General Public License for more details.
 #
 #  This copyright notice MUST APPEAR in all copies of the script!
 #
