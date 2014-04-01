17133a7cce9ca640fc895de4b221a6740a148aef
Alexandr.Dubovikov <Alexandr.Dubovikov@gmail.com>
2014-04-01 12:43:23 +0000

    fixed expand search

--- webhomer/cflow.php
+++ webhomer/cflow.php
@@ -123,7 +123,7 @@ if ( count($location) <= 1 ) {
 $pcapurl = $_SERVER["QUERY_STRING"];
 $pcapjson = json_encode($_REQUEST);
 
-$complete_url = preg_replace('/from_time=(.*)\&callid/', 'callid', $_SERVER["QUERY_STRING"]);
+$complete_url = preg_replace('/to_time=(.*)\&callid/', 'callid', $_SERVER["QUERY_STRING"]);
 
 if (isset($flow_from_date, $flow_from_time, $flow_to_time, $flow_to_date))
 {
