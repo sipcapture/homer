0c8f4f1687d9d4d25a56370e3809dc7567b65cbc
Alexandr.Dubovikov <Alexandr.Dubovikov@gmail.com>
2014-09-15 07:20:57 +0000

    removed sql create table

--- scripts/partrotate_unixtimestamp.pl
+++ scripts/partrotate_unixtimestamp.pl
@@ -63,61 +63,8 @@ $auth_column = "authorization" if($sql_schema_version == 1);
 
 #$db->{PrintError} = 0;
 
-$sql = "CREATE TABLE IF NOT EXISTS `".$mysql_table."` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `micro_ts` bigint(18) NOT NULL DEFAULT '0',
-  `method` varchar(50) NOT NULL DEFAULT '',
-  `reply_reason` varchar(100) NOT NULL,
-  `ruri` varchar(200) NOT NULL DEFAULT '',
-  `ruri_user` varchar(100) NOT NULL DEFAULT '',
-  `from_user` varchar(100) NOT NULL DEFAULT '',
-  `from_tag` varchar(64) NOT NULL DEFAULT '',
-  `to_user` varchar(100) NOT NULL DEFAULT '',
-  `to_tag` varchar(64) NOT NULL,
-  `pid_user` varchar(100) NOT NULL DEFAULT '',
-  `contact_user` varchar(120) NOT NULL,
-  `auth_user` varchar(120) NOT NULL,  
-  `callid` varchar(100) NOT NULL DEFAULT '',
-  `callid_aleg` varchar(100) NOT NULL DEFAULT '',
-  `via_1` varchar(256) NOT NULL,
-  `via_1_branch` varchar(80) NOT NULL,
-  `cseq` varchar(25) NOT NULL,
-  `diversion` varchar(256) NOT NULL,
-  `reason` varchar(200) NOT NULL,
-  `content_type` varchar(256) NOT NULL,
-  `".$auth_column."` varchar(120) NOT NULL,
-  `user_agent` varchar(256) NOT NULL,
-  `source_ip` varchar(60) NOT NULL DEFAULT '',
-  `source_port` int(10) NOT NULL,
-  `destination_ip` varchar(60) NOT NULL DEFAULT '',
-  `destination_port` int(10) NOT NULL,
-  `contact_ip` varchar(60) NOT NULL,
-  `contact_port` int(10) NOT NULL,
-  `originator_ip` varchar(60) NOT NULL DEFAULT '',
-  `originator_port` int(10) NOT NULL,
-  `proto` int(5) NOT NULL,
-  `family` int(1) DEFAULT NULL,
-  `rtp_stat` varchar(256) NOT NULL,
-  `type` int(2) NOT NULL,
-  `node` varchar(125) NOT NULL,
-  `msg` text NOT NULL,
-  PRIMARY KEY (`id`,`date`),
-  KEY `ruri_user` (`ruri_user`),
-  KEY `from_user` (`from_user`),
-  KEY `to_user` (`to_user`),
-  KEY `pid_user` (`pid_user`),
-  KEY `auth_user` (`auth_user`),
-  KEY `callid_aleg` (`callid_aleg`),
-  KEY `date` (`date`),
-  KEY `callid` (`callid`),
-  KEY `method` (`method`),
-  KEY `source_ip` (`source_ip`),
-  KEY `destination_ip` (`destination_ip`)
-) ENGINE=".$engine." DEFAULT CHARSET=utf8 $compress
-PARTITION BY RANGE ( UNIX_TIMESTAMP(`date`)) (PARTITION pmax VALUES LESS THAN MAXVALUE ENGINE = ".$engine.")";
-
-my $sth = $db->do($sql) if($check_table == 1);
+#we have now multiple tables with different schema. Better do it in sql script 
+#
 
 #check if the table has partitions. If not, create one
 my $query = "SHOW TABLE STATUS FROM ".$mysql_dbname. " WHERE Name='".$mysql_table."'";
