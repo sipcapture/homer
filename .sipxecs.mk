3023bf91f3df2c992866ff89d4e7eac6bcb75950
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2014-11-19 13:22:20 +0100

    Delete .sipxecs.mk

--- .sipxecs.mk
+++
@@ -1,5 +0,0 @@
-homer_VER = 3.2.5
-homer_REV = 2
-homer_SRPM = homer-$(homer_VER)-$(homer_REV)$(RPM_DIST).src.rpm
-homer_SPEC = $(SRC)/$(PROJ)/homer.spec
-homer_SOURCES = $(BUILDDIR)/$(PROJ)/homer-$(homer_VER).tar.gz
