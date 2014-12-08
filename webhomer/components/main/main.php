5b1f4ae84800c8dc218bc9d04dff8d77abc1e042
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2014-12-08 10:43:23 +0100

    fixed logic

--- webhomer/components/main/main.php
+++ webhomer/components/main/main.php
@@ -59,7 +59,7 @@ class HomerMain {
            HTML_mainhtml::displayBackground($bglogo);
 
            /* For login we do nothing */
-           if(!defined('NOAUTH' && $component == "login")) return;
+           if(!defined('NOAUTH') || NOAUTH == 0) && $component == "login") return;
                   
            /* Check userlevel */   
            foreach($components as $key=>$value) {             
