4e141b7d0f75936d80713435102b9141394fdc1c
sipcapture <sipcapture@centos-7_x86_64.test.homer.local>
2015-11-30 06:16:25 +0100

    FIX: kamailio subversion autodetecion

--- mak/kamailio.deps.mk
+++ mak/kamailio.deps.mk
@@ -1,5 +1,5 @@
-kamailio_VER = 4.3.4
-kamailio_TAG = 4.3.4
+kamailio_VER = $(shell cd $(SRC)/kamailio; grep -A4 ' version number' Makefile.defs | grep -E '^VERSION' | sed 's/ //g' | sed 's/VERSION=//g' ).$(shell cd $(SRC)/kamailio; grep -A4 ' version number' Makefile.defs | grep -E '^PATCHLEVEL' | sed 's/ //g' | sed 's/PATCHLEVEL=//g' ).$(shell cd $(SRC)/kamailio; grep -A4 ' version number' Makefile.defs | grep -E '^SUBLEVEL' | sed 's/ //g' | sed 's/SUBLEVEL=//g' )$(shell cd $(SRC)/kamailio; grep -A4 ' version number' Makefile.defs | grep -E '^EXTRAVERSION' | sed 's/ //g' | sed 's/EXTRAVERSION=//g' )
+kamailio_TAG = $(kamailio_VER)
 kamailio_BRANCH = 4.3
 kamailio_PACKAGE_REVISION = $(shell cd $(SRC)/kamailio; git checkout $(kamailio_BRANCH) >/dev/null 2>&1; ../config/revision-gen $(kamailio_TAG))$(RPM_DIST)
 kamailio_SRPM = kamailio-$(kamailio_VER)-$(kamailio_PACKAGE_REVISION).src.rpm
