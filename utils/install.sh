bf092a6256866eff2615bfaba542e8499a58214e
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-04-09 23:16:57 +0200

    point installer to kamailio44 repo

--- utils/install.sh
+++ utils/install.sh
@@ -161,8 +161,8 @@ case $DIST in
 		apt-get update && apt-get install -y mysql-server-5.7 libmysqlclient18
 		# Kamailio + sipcapture module
 		apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xfb40d3e6508ea4c8
-		echo "deb http://deb.kamailio.org/kamailio jessie main" >> /etc/apt/sources.list
-		echo "deb-src http://deb.kamailio.org/kamailio jessie main" >> /etc/apt/sources.list
+		echo "deb http://deb.kamailio.org/kamailio44 jessie main" >> /etc/apt/sources.list
+		echo "deb-src http://deb.kamailio.org/kamailio44 jessie main" >> /etc/apt/sources.list
 		apt-get update && apt-get install -f -yqq kamailio rsyslog kamailio-outbound-modules kamailio-geoip-modules kamailio-sctp-modules kamailio-tls-modules kamailio-websocket-modules kamailio-utils-modules kamailio-mysql-modules kamailio-extra-modules geoip-database geoip-database-extra
 
 
