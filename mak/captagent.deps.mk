1fd9fea58230483bd4ab4980e2dbe89151b58cc8
Konstantin S. Vishnivetsky <voip-projects@vishnivetsky.ru>
2017-03-07 15:28:58 +0600

    Add: captagent version/tar fixups.

--- mak/captagent.deps.mk
+++ mak/captagent.deps.mk
@@ -2,14 +2,14 @@
 # Licensed to the User under the GPL license.
 #
 
-captagent_VER = 6.0.2
-captagent_TAG = 6.0.2
-captagent_BRANCH = captagent6
+captagent_VER = 6.2.0.2
+captagent_TAG = 6.2.0.2
+captagent_BRANCH = master
 captagent_PACKAGE_REVISION = $(shell cd $(SRC)/captagent; git checkout $(captagent_BRANCH) >/dev/null 2>&1; ../config/revision-gen $(captagent_TAG))$(RPM_DIST)
 captagent_SRPM = captagent-$(captagent_VER)-$(captagent_PACKAGE_REVISION).src.rpm
 captagent_TAR = captagent/captagent-$(captagent_VER).tar.gz
 captagent_DEB = deb
-captagent_APT_SETUP = libmysqlclient-dev libexpat1-dev bison flex libpcap-dev libjson-c-dev
+captagent_APT_SETUP = libmysqlclient-dev libexpat1-dev bison flex libpcap-dev libjson-c-dev libuv-dev
 
 captagent_SRPM_DEFS = \
 	--define "BUILD_NUMBER $(captagent_PACKAGE_REVISION)" \
