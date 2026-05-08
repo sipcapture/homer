89c530a60ee1fa5a8a967caf3baf9234f26e70d3
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2026-05-08 20:02:27 +0200

    chore: bump version to 11.0.201

--- src/version.go
+++ src/version.go
@@ -24,7 +24,7 @@ import (
 // Version information for homer-core
 var (
 	// VERSION_APPLICATION is the application version
-	VERSION_APPLICATION = "11.0.200"
+	VERSION_APPLICATION = "11.0.201"
 
 	// BuildDate is the build date
 	BuildDate = ""
