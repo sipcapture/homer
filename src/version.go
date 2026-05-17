a31c6f2f831bf5676328ab6a215cd73340957235
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2026-05-17 23:05:14 +0200

    chore: release 11.0.217

--- src/version.go
+++ src/version.go
@@ -24,7 +24,7 @@ import (
 // Version information for homer-core
 var (
 	// VERSION_APPLICATION is the application version
-	VERSION_APPLICATION = "11.0.216"
+	VERSION_APPLICATION = "11.0.217"
 
 	// BuildDate is the build date
 	BuildDate = ""
