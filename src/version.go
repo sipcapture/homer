15201e6c6b72ec12a2153b92c4a138c1132b6ed9
Alexandr Dubovikov <heplabsgmbh@gmail.com>
2026-07-09 00:31:30 +0200

    chore(release): bump version to 11.0.288
    Release fix for data_extra JSON string comparison (closes #857).
    
    👹 Fixed by Goblin

--- src/version.go
+++ src/version.go
@@ -24,7 +24,7 @@ import (
 // Version information for homer-core
 var (
 	// VERSION_APPLICATION is the application version
-	VERSION_APPLICATION = "11.0.287"
+	VERSION_APPLICATION = "11.0.288"
 
 	// BuildDate is the build date
 	BuildDate = ""
