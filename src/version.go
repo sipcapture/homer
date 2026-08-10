00cbd34a12c1cef721c25e86b55d862c400144dc
rita7lopes <ritamlopes@live.com.pt>
2026-08-10 14:29:09 -0500

    chore: bump version to 11.0.314

--- src/version.go
+++ src/version.go
@@ -24,7 +24,7 @@ import (
 // Version information for homer-core
 var (
 	// VERSION_APPLICATION is the application version
-	VERSION_APPLICATION = "11.0.313"
+	VERSION_APPLICATION = "11.0.314"
 
 	// BuildDate is the build date
 	BuildDate = ""
